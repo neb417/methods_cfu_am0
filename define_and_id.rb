a9d593f77f076b25a59d03be85272cde3bb3995a
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-06-09 14:38:39 -0600

    Initial commit

--- define_and_id.rb
+++ define_and_id.rb
@@ -6,22 +6,27 @@
 # No arguments are passed; downcase has one clear job which is to lowercase all letters that exist in the String
 # The return value is "hello world"
 "Hello World".downcase
+#the downcase method is being called on the string "Hello World" which will turn all letters to lower case.
 
 # YOU DO the rest:
 
 
 "Hello World".include?("Hello")
-
+#the include question mark method is being called on the string "Hello World" asking if the string "Hello" is included in the original string.
+#the output should be true.
 
 "Hello World".end_with?("Hello")
-
+#the end_with question mark method is being called on the string "Hello World" asking if the string "Hello" ends the original string.
+#the output should be false.
 
 "Hello World".end_with?("rld")
-
+#the end_with question mark method is being called on the string "Hello World" asking if the string "rld" ends the original string.
+#the output should be true.
 
 12.even?
+#the even question mark method is being called on the Integer 12 asking if the Integer is even.
+#the output should be true
 
 
 18.next
-
-
+#the next method is being called on the Integer 18 which will output the next Integer that is numerically larger, 19.
