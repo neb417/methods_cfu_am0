172b3f77b05be0bb2f131be86d53cfd8f24ca7b8
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-06-09 15:36:11 -0600

    Commit four methods not used with explanation

--- methods_variables.rb
+++ methods_variables.rb
@@ -30,9 +30,22 @@ p string1.length
 user_name = "coco_11am"
 last_login = "12/09/2021"
 
-# 1: In the Ruby Docs for String methods (https://ruby-doc.org/core-3.1.0/String.html), find 4 methods you have not yet used and call them on one of the variables above. Between reading the documentation and reading the output from calling the methods, make sense of what they do.
+# 1: In the Ruby Docs for String methods (https://ruby-doc.org/core-3.1.0/String.html),
+# find 4 methods you have not yet used and call them on one of the variables above.
+#Between reading the documentation and reading the output from calling the methods, make sense of what they do.
   # Write the methods out on the lines below, with an explanation in your own words of how they work.
+user_name.insert(0,last_login)
+# the insert method is being called on the variable user_name to insert the variable last_login at index 0.
+# this alse redefines the variable user_name as "12/09/2021coco_11am"
+user_name.swapcase
+#the swapcase method is being called to make all lower case letter uppers case and visa versa on the variable user_name.
+last_login.bytes
+#the bytes method is being called on the last_login variable to output an array of how many bytes each digit is.
+last_login.chr
+#the chr method is being called on the last_login variable to output the first character in the string.
 
 
 
-# 2: Do some research (either testing out with your own code or Googling) to build an understanding of what the `!` does, when at the end of a method name. Show your understading by providing an example and writing an explanation.
+# 2: Do some research (either testing out with your own code or Googling) to build an understanding of what the `!` does,
+# when at the end of a method name.
+# Show your understading by providing an example and writing an explanation.
